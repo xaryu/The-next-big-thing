c82a58d7585be30b0e7646d2b61116699e1ed953
xaryu <himselfpaul94@gmail.com>
2014-07-03 01:54:24 +0300

    added the bone structure

---
+++ routes/index.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res) {
+  res.render('index', { title: 'Express' });
+});
+
+/* GET HelloWorld page */
+
+router.get('/helloworld', function(req,res){
+	res.render('helloworld',{ title: 'tagtag' })
+});
+
+/* GET Userlist page */
+router.get('/userlist',function(req,res){
+	var db=req.db;
+	var collection = db.get('usercollection');
+	collection.find({},{},function(e,docs){
+		res.render('userlist', {
+			"userlist" : docs
+		});
+	});
+});
+
+
+module.exports = router;
+
